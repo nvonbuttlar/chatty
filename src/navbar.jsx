7be7c06a8bfaa8780e85ba436272c14e7351e68d
vagrant <nick.von.buttlar@gmail.com>
2018-11-06 22:17:13 +0000

    add styling

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -6,7 +6,7 @@ class NavBar extends Component {
     return (
       <nav className="navbar">
         <a href="/" className="navbar-brand">Chit-Chat</a>
-        <span>Users Connected: {this.props.userCount}</span>
+        <span className="navbar-connected">Users Connected: {this.props.userCount}</span>
       </nav>
     )
   }
